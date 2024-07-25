652b8f4d11947dada5acd719c75cd6cfa99339ea
Seungkwan Bang <113026591+RoyBangdlsu@users.noreply.github.com>
2024-07-25 21:41:56 +0800

    testing file update
    to see the time consumption of connection

---
+++ testing/test.js
@@ -0,0 +1,44 @@
+// this file is to test the program. you can uncomment it and paste it in our program to test.
+
+
+
+
+// app.post('/register', async (req, res) => {
+//     const start = Date.now();
+//     // Registration logic
+//     const end = Date.now();
+//     console.log(`User registration took ${end - start} ms`);
+//   });
+  
+//   app.post('/login', async (req, res) => {
+//     const start = Date.now();
+//     // Login logic
+//     const end = Date.now();
+//     console.log(`User login took ${end - start} ms`);
+//   });
+
+  
+
+//   app.post('/add-exercise', async (req, res) => {
+//     const start = Date.now();
+//     // Exercise addition logic
+//     const end = Date.now();
+//     console.log(`Adding exercise took ${end - start} ms`);
+//   });
+  
+//   app.post('/log-workout', async (req, res) => {
+//     const start = Date.now();
+//     // Workout logging logic
+//     const end = Date.now();
+//     console.log(`Logging workout took ${end - start} ms`);
+//   });
+
+  
+//   app.get('/view-progress', async (req, res) => {
+//     const start = Date.now();
+//     // Data retrieval logic
+//     const end = Date.now();
+//     console.log(`Retrieving progress data took ${end - start} ms`);
+//     res.render('view-progress', { ... });
+//   });
+  
